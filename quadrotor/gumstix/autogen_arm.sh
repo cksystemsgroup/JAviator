b9ac3ed0e9f391d02a372d1b26c1cb5707411c1f
Harald Roeck <hroeck@gmail.com>
2009-05-13 12:57:26 +0200

    use pthread lib

--- quadrotor/gumstix/autogen_arm.sh
+++ quadrotor/gumstix/autogen_arm.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 
-CROSS_TOOLS=/usr/local/openmoko/arm/bin/arm-angstrom-linux-gnueabi
-TARGET=arm-angstrom-linux-gnueabi
+source cross.inc
 
-CFLAGS="-Wall -O2"
-PATH=$CROSS_TOOLS:$PATH
-
-AS=${CROSS_TOOLS}-as
-CC=${CROSS_TOOLS}-gcc
-CXX=${CROSS_TOOLS}-g++
-LD=${CROSS_TOOLS}-ld
-NM=${CROSS_TOOLS}-nm
-OBJDUMP=${CROSS_TOOLS}-objdump
-AR=${CROSS_TOOLS}-ar
-RANLIB=${CROSS_TOOLS}-ranlib
-STRIP=${CROSS_TOOLS}-strip
-
-./configure --host=i686-pc-linux-gnu --target=arm-angstrom-linux-gnueabi CC=$CC CFLAGS="$CFLAGS"
+./configure --host=i686-pc-linux-gnu --target=${TARGET} CC=$CC CFLAGS="$CFLAGS"
 
