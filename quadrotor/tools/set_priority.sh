7273c2df4fc6375b4ca2a294b20dc739ebef85be
Rainer Trummer <***>
2014-05-07 10:12:48 +0200

    gumstix priority values changed

--- quadrotor/tools/set_priority.sh
+++ quadrotor/tools/set_priority.sh
@@ -1,46 +1,40 @@
 #!/bin/sh
 
+./control -c
+
 # this is busybox style:
 #    ps
 # this is GNU style:
 #    px ax 
 
 PS_CMD="ps"
-WLAN_PRIO=40
-
-./control -c
-
 tmpfile="/tmp/ps_file.txt"
 
-# use the correct ps command:
-
-# this is gnu style
+# use the correct PS command
 ${PS_CMD} > $tmpfile
 
-# find the pids in the ps file
-#SERIAL_PID=$(grep "IRQ-20" $tmpfile | awk '{print $1}' | head -n 1)
+# find the PIDs in the tmp file
+SERIAL_PID=$(grep "IRQ-20" $tmpfile | awk '{print $1}' | head -n 1)
+WLAN_PID=$(grep "wlan_main_servi" $tmpfile | awk '{print $1}' | head -n 1)
+WLAN_IRQ=$(grep "wlan_reassoc_se" $tmpfile | awk '{print $1}' | head -n 1)
+NET_RX=$(grep "softirq-net-rx/" $tmpfile | awk '{print $1}' | head -n 1)
+NET_TX=$(grep "softirq-net-tx/" $tmpfile | awk '{print $1}' | head -n 1)
 
-SPI_IRQ=$(grep "\[IRQ-16\]" $tmpfile | awk '{print $1}' | head -n 1)
-PXA_PID=$(grep "pxa2xx-spi" $tmpfile | awk '{print $1}' | head -n 1)
-WLAN_PID=$(grep "wlan_main" $tmpfile | awk '{print $1}' | head -n 1)
-WLAN_IRQ=$(grep "IRQ-160" $tmpfile | awk '{print $1}' | head -n 1)
-NET_RX=$(grep "net-rx" $tmpfile | awk '{print $1}' | head -n 1)
-NET_TX=$(grep "net-tx" $tmpfile | awk '{print $1}' | head -n 1)
+chrt -f -p 95 $SERIAL_PID
+chrt -f -p 40 $WLAN_PID
+chrt -f -p 40 $WLAN_IRQ
+chrt -f -p 90 $NET_RX
+chrt -f -p 90 $NET_TX
 
-#chrt -f -p 95 $SERIAL_PID 
-#chrt -p $SERIAL_PID 
-chrt -f -p 95 $SPI_IRQ
-chrt -f -p 90 $PXA_PID
-chrt -f -p ${WLAN_PRIO} $WLAN_PID
-chrt -f -p ${WLAN_PRIO} $WLAN_IRQ
-chrt -f -p ${WLAN_PRIO} $NET_RX
-chrt -f -p ${WLAN_PRIO} $NET_TX
-
-chrt -p $SPI_IRQ
-chrt -p $PXA_PID
+echo "SERIAL PID:"
+chrt -p $SERIAL_PID
+echo "WLAN PID:"
 chrt -p $WLAN_PID
+echo "WLAN IRQ:"
 chrt -p $WLAN_IRQ
+echo "NET RX:"
 chrt -p $NET_RX
+echo "NET TX:"
 chrt -p $NET_TX
 
 rm $tmpfile
